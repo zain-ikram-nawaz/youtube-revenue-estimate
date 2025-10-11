33c02babea697aac19b12381d0b903b7daf4610b
Zain ikram <zainikram704@gmail.com>
2025-10-11 12:47:22 +0500

    add dynamic

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { connectDB } from "../../lib/db"
 import Blog from "../../../models/Blog"
 import { NextResponse } from "next/server";
