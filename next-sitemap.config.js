1f413ea94053b4d31fa4adabadd65b44254a6031
Zain Ikram <zainikram704@gmail.com>
2025-10-25 10:24:25 +0500

    addsitemanp

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,5 +6,39 @@ module.exports = {
   changefreq: 'daily',
   priority: 0.7,
   autoLastmod: true,
-  exclude: ['/404', '/admin'],
+
+  // ❌ Ye pages & folders sitemap se bahar rahenge
+  exclude: [
+    '/404',
+    '/admin',
+    '/components/*',
+    '/src/*',
+    '/_app',
+    '/_document',
+    '/models',
+    '/middelware',
+    '/form',
+    '/hooks',
+    '/lib',
+    '/register',
+
+  ],
+  // ✅ Add manually included pages (force include homepage)
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'),
+  ],
+
+  // ✅ robots.txt config (optional but recommended)
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/components/', '/src/', '/_next/', '/api/','middleware','models'],
+      },
+    ],
+    additionalSitemaps: [
+      'https://channelincome.com/sitemap.xml',
+    ],
+  },
 };
